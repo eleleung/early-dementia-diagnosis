1553852b91add0c39ae404699ad048f6f470d645
stan13 <nathanastanley@gmail.com>
2017-03-27 03:22:15 +0800

    Got the jwt token based authentication working and enabled on methods by default

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,7 +1,8 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from .serializers import UserSerializer
 
@@ -9,6 +10,19 @@ class AuthRegister(APIView):
     serializer_class = UserSerializer
     permission_classes = (AllowAny, )
 
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AuthUpdate(APIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (JSONWebTokenAuthentication, )
+
+    #this method isn't working at the moment due to the existing user error
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
