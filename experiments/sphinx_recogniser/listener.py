d287350884e03558b0f4ee4f43e99c13fe06a766
Caitlin2694 <21324675@student.uwa.edu.au>
2017-04-02 16:32:00 +0800

    Sphinx recognition (with and without Python Speech Recognition)

---
+++ experiments/sphinx_recogniser/listener.py
@@ -0,0 +1,48 @@
+
+#TO SET UP
+
+# Make sure we have up-to-date versions of pip, setuptools and wheel:
+#$ pip install --upgrade pip setuptools wheel
+#$ pip install --upgrade pocketsphinx
+
+'''import os
+from pocketsphinx import LiveSpeech, get_model_path
+
+model_path = get_model_path()
+
+speech = LiveSpeech(
+    verbose=False,
+    sampling_rate=16000,
+    buffer_size=2048,
+    no_search=False,
+    full_utt=False,
+    hmm=os.path.join(model_path, 'en-us'),
+    lm=os.path.join(model_path, 'en-us.lm.bin'),
+    dic=os.path.join(model_path, 'cmudict-en-us.dict')
+)
+
+for phrase in speech:
+    print(phrase)'''
+
+#!/usr/bin/env python3
+# Requires PyAudio and PySpeech.
+
+import speech_recognition as sr
+import pocketsphinx
+
+# Record Audio
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+# Speech recognition using Google Speech Recognition
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print("You said: " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Sphinx Recognition service; {0}".format(e))
