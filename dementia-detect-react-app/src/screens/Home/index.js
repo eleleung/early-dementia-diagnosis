b2570175ae240ad12b67b3ff97ff1b94ef186ede
stan13 <nathans@miclub.com.au>
2017-10-27 03:19:24 +0800

    Mobile app home screen

--- dementia-detect-react-app/src/screens/Home/index.js
+++ dementia-detect-react-app/src/screens/Home/index.js
@@ -7,7 +7,9 @@ import {
   Button,
   TextInput,
   StyleSheet,
-  ScrollView
+  ScrollView,
+  Image,
+  Dimensions
 } from 'react-native';
 import { inject, observer } from 'mobx-react/native';
 import { List, ListItem, Icon } from 'react-native-elements';
@@ -15,6 +17,8 @@ import { List, ListItem, Icon } from 'react-native-elements';
 import NavButtons from '../../global/NavButtons';
 import NavBar     from '../../global/NavBar';
 import Constants  from '../../global/Constants';
+
+const win = Dimensions.get('window');
 
 @inject('App', 'User') @observer
 class Home extends Component {
@@ -31,20 +35,60 @@ class Home extends Component {
 
         return (
             <ScrollView style={style.container}>
-                <Text>Your stats</Text>
-                <List containerStyle={{ marginBottom: 20 }}>
+                <Text style={style.heading}>Your Stats</Text>
+                <List>
                     <ListItem
                         title={'Completed Tests'}
-                        leftIcon={{name: 'puzzle'}}
-                        rightIcon={{name: 'a'}}
-                        badge={{ value: 3, textStyle: { color: 'white' }, containerStyle: {  } }}
-
-                    />
+                        leftIcon={{name: 'done'}}
+                        hideChevron={true}
+                        badge={{ value: 0, textStyle: { color: 'white' }, containerStyle: { backgroundColor: 'green' } }}/>
+                    <ListItem
+                        title={'Assigned Tests'}
+                        leftIcon={{name: 'assessment'}}
+                        hideChevron={true}
+                        badge={{ value: 0, textStyle: { color: 'white' }, containerStyle: { backgroundColor: 'orange' } }}/>
                 </List>
 
-                <Text>News</Text>
+                <Text style={style.heading}>News</Text>
+                <List >
+                    <ListItem
+                        title={'Blood-thinning drugs can reduce risk of dementia by up to 48%'}
+                        titleNumberOfLines={2}
+                        subtitle={
+                            <View style={{marginTop: 10}}>
+                                <Image style={{width: win.width - 20, height: 200, }} source={require('../../../img/blood.jpg')}/>
+                                <Text style={{marginTop:5}}>Research ‘strongly suggests’ that patients taking anticoagulants for irregular heartbeat could be protected against dementia and stroke</Text>
+                            </View>
+                        }
+                        subtitleNumberOfLines={4}/>
+                    <ListItem
+                        title={'Anti-stroke drugs may cut dementia risk by half'}
+                        titleNumberOfLines={2}
+                        subtitle={
+                            <View style={{marginTop: 10}}>
+                                <Image style={{width: win.width - 20, height: 200, }} source={require('../../../img/drugs.jpg')}/>
+                                <Text style={{marginTop:5}}>Blood-thinning drugs taken to prevent strokes may dramatically reduce the risk of dementia, a study has found.</Text>
+                            </View>
+                        }
+                        subtitleNumberOfLines={4}/>
+                    <ListItem
+                        leftIcon={{name: 'library-books'}}
+                        title={'More'}/>
+                </List>
 
-                <Text>Trials</Text>
+                <Text style={style.heading}>Upcoming Trials</Text>
+                <List  containerStyle={{ marginBottom: 20 }}>
+                    <ListItem
+                        title={'Sodium Selenate as a treatment for patients with possible behavioural-variant Fronto-Temporal Dementia'}
+                        titleNumberOfLines={3}
+                    />
+                    <ListItem
+                        title={'The effect of high-intensity exercise on cognitive function in healthy older adults'}
+                        titleNumberOfLines={3}/>
+                    <ListItem
+                        leftIcon={{name: 'library-books'}}
+                        title={'More'}/>
+                </List>
             </ScrollView>
         )
     }
@@ -55,6 +99,13 @@ const style = StyleSheet.create({
         flex: 1,
         backgroundColor: Constants.Colors.backgroundColor,        
     },    
+    heading: {
+        marginTop: 20,
+        fontFamily: 'Helvetica Neue',
+        fontSize: 16,
+        textAlign: 'center',
+        paddingLeft: 10,
+    },
 });    
 
 export default Home;
